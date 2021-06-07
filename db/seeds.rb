9a7c60b9e0f8ba939037b1b0cd6fe4a812a797c7
Asahi20 <asahiyamaguchi1120@gmail.com>
2021-06-07 23:23:51 +0900

    いいね機能の実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+email = "test@example.com"
+password = "password"
 
 
 # users テーブルを再生成（関連付いたテーブルを含む）
@@ -15,11 +17,19 @@ user1 = User.create!(email: "satou@example.com", password: "password")
 user2 = User.create!(email: "suzuki@example.com", password: "password")
 user3 = User.create!(email: "tanaka@example.com", password: "password")
 
-user2.posts.create!(content: "沖縄そば")
-user1.posts.create!(content: "海ぶどう")
-user3.posts.create!(content: "ゴーヤーチャンプルー")
-user3.posts.create!(content: "サーターアンダギー")
-user1.posts.create!(content: "タコライス")
+post1 = user2.posts.create!(content: "沖縄そば")
+post2 = user1.posts.create!(content: "海ぶどう")
+post3 = user3.posts.create!(content: "ゴーヤーチャンプルー")
+post4 = user3.posts.create!(content: "サーターアンダギー")
+post5 = user1.posts.create!(content: "タコライス")
+
+post1.likes.create!(user_id: user1.id)
+post1.likes.create!(user_id: user3.id)
+post2.likes.create!(user_id: user2.id)
+post3.likes.create!(user_id: user1.id)
+post4.likes.create!(user_id: user1.id)
+post4.likes.create!(user_id: user2.id)
+post5.likes.create!(user_id: user3.id)
 
 # ログイン時に使用するアカウント（変数への代入は不要）
 User.create!(email: email, password: password)
